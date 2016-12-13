fca829c85a7b532fc60d6a4fdb9553252e308981
Terry Moore II <terry.moore.ii@gmail.com>
2016-12-12 19:54:25 -0500

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.json({type: 'application/json'}));
+
+// [START YourAction]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    assistant.tell('Hello, World!');
+  }
+
+  assistant.handleRequest(responseHandler);
+});
+// [END YourAction]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
+
+module.exports = app;
