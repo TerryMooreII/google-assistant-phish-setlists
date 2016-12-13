16156098750b279ad67763cb11c952bcab78d9b9
Terry Moore II <terry.moore.ii@gmail.com>
2016-12-12 20:38:25 -0500

    Working phish call

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ process.env.DEBUG = 'actions-on-google:*';
 let Assistant = require('actions-on-google').ApiAiAssistant;
 let express = require('express');
 let bodyParser = require('body-parser');
-var pnet = require('pnet');
+//var pnet = require('pnet');
 
 const PHISH_NET_API_KEY = 'CEE681B57889948C24DE';
 const PHISH_API = 'http://api.phish.net/';
-pnet.apikey(PHISH_NET_API_KEY);
+//pnet.apikey(PHISH_NET_API_KEY);
+
+var Taboot = require('taboot');
+var pnet = new Taboot(PHISH_NET_API_KEY).pnet
 
 let app = express();
 app.use(bodyParser.json({type: 'application/json'}));
@@ -18,17 +21,27 @@ app.use(bodyParser.json({type: 'application/json'}));
 app.post('/', function (req, res) {
   const assistant = new Assistant({request: req, response: res});
 
-  pnet.get('pnet.shows.setlists.latest', function(err, url, parsedJSONResource){
-    // Fulfill action business logic
-    console.log(parsedJSONResource);
-    
-    function responseHandler (assistant) {
-      // Complete your fulfillment logic and send a response
-      assistant.tell('Hello, World!');
-    }
+    pnet.shows.setlists.latest({}, function(err, data) {
+        
+        /*
+        showdate: '2016-10-31',
+     showyear: '2016',
+     city: 'Las Vegas',
+     state: 'NV',
+     country: 'USA',
+     venue: 'MGM Grand Garden Arena',
+     */
+     var venue = data[0].venue;
+     var date = data[0].showdate;
 
-    assistant.handleRequest(responseHandler);
-  });
+     function responseHandler (assistant) {
+       // Complete your fulfillment logic and send a response
+       assistant.tell(`The last show was at ${venue} on ${date}`);
+     }
+
+     assistant.handleRequest(responseHandler);
+
+     });
 
 
 });
